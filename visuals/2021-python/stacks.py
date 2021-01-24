1ae10b02d62ac06b0066a31506e6b3fd8008b938
Ben Rutter <benrrutter@gmail.com>
2021-01-24 14:52:47 +0000

    2 pieces added

--- visuals/2021-python/stacks.py
+++ visuals/2021-python/stacks.py
@@ -1,11 +1,11 @@
 import shades
 import random
 
-canvas = shades.Canvas()
-shade_one = shades.BlockColor((102, 169, 193))
-shade_two = shades.BlockColor((30, 86, 165))
-shade_three = shades.BlockColor((236, 147, 155))
-shade_four = shades.BlockColor((127, 89, 155))
+canvas = shades.Canvas(1500, 1000)
+shade_one = shades.BlockColor((97, 196, 227))
+shade_two = shades.BlockColor((247, 228, 91))
+shade_three = shades.BlockColor((251, 5, 28))
+shade_four = shades.BlockColor((23, 79, 114))
 
 def stack(start_coords, width, height, offset):
   perc = height/100
@@ -19,7 +19,7 @@ def stack(start_coords, width, height, offset):
   for y in range(int(start_coords[1]), int(start_coords[1]+height), 5):
     filtered_dict = {k: shade_dict[k] for k in shade_dict if k > y}
     color = filtered_dict[list(filtered_dict.keys())[0]]
-    color.line(canvas, (start_coords[0], y), (start_coords[0] + width, y + offset), 1)
+    color.line(canvas, (start_coords[0], y), (start_coords[0] + width, y + offset), 2)
 
 margin = 50
 width = 20
