93cb96e70f65c628d1fd789cee7efe2501503dd0
Ben <55319792+benrutter@users.noreply.github.com>
2022-02-23 13:20:12 +0000

    Update circles.py

--- visuals/2022-python/circles.py
+++ visuals/2022-python/circles.py
@@ -1,18 +1,32 @@
 import shades
-from random import randint
+import random
 
-canvas = shades.Canvas()
+canvas = shades.Canvas(1500, 1500)
+noise = shades.NoiseField(scale=0.02)
+ink = shades.BlockColor()
 
-ink = shades.NoiseGradient(
-    warp_size=250,
-    color=(200, 200, 200),
-    noise_fields=[shades.NoiseField(scale=0.002) for i in range(3)],
-)
-for i in range(100):
-    ink.warp_noise = [shades.NoiseField(scale=0.002), shades.NoiseField(scale=0.02)] 
-    width = randint(25, 100)
-    x = randint(0, canvas.width)
-    for y in range(-500, canvas.height+500, 15):
-        ink.circle_outline(canvas, (x, y), width, 1)
+colors = [
+    (249, 65, 68),
+    (243, 114, 44),
+    (248, 150, 30),
+    (249, 132, 74),
+    (249, 199, 79),
+    (144, 190, 109),
+    (67, 170, 139),
+    (77, 144, 142),
+    (87, 117, 144),
+    (39, 125, 161),
+]
+
+for y in range(0, canvas.height, 5):
+    segments = 15
+    for i in range(segments):
+        x = (canvas.width/segments) * i
+        if i % 2:
+            size = canvas.width/segments/2 * noise.noise((x, y))
+        else:
+            size = canvas.width/segments/2 * (1 - noise.noise((x, y)))
+        ink.color = random.choice(colors)
+        ink.circle_outline(canvas, (x, y), size, 1)
 
 canvas.show()
